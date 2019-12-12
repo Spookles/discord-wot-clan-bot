4520534209366cef47a55eef3244001df289f89a
Simon <simon.brol@outlook>
2019-12-12 23:40:19 +0100

    Discord bot functionality added

--- clan.py
+++ clan.py
@@ -1,7 +1,49 @@
+import requests
+import json
+
 class Clan:
     #var
-    id = ""
+    #General information
+    id = 0
     name = ""
     tag = ""
     emblem_url = ""
+    color = ""
+    created_at = ""
+    description = ""
+    members_count = 0
+    motto = ""
+    #Stronghold statistics
+    skirmish = {
+        'fb_6': {'value': '', 'rank': '', 'rank_delta': ''},
+        'fb_8': {'value': '', 'rank': '', 'rank_delta': ''},
+        'fb_10': {'value': '', 'rank': '', 'rank_delta': ''}
+    }
+    global_map = {
+        'gm_6': {'value': '', 'rank': '', 'rank_delta': ''},
+        'gm_8': {'value': '', 'rank': '', 'rank_delta': ''},
+        'gm_10': {'value': '', 'rank': '', 'rank_delta': ''}
+    }
+
     #functions
+    def getDetails(self, id):
+        api_url = "https://api.worldoftanks.eu/wot/clans/info/?application_id=0ecfda2435a084d16fa9e02ea75ee0db&clan_id=%s" % (id)
+        response = requests.get(api_url)
+        json = response.json()['data'][str(id)]
+        self.color = json['color']
+        self.created_at = json['created_at']
+        self.description = json['description']
+        self.members_count = json['members_count']
+        self.motto = json['motto']
+
+    def getRating(self, id):
+        api_url = "https://api.worldoftanks.eu/wot/clanratings/clans/?application_id=0ecfda2435a084d16fa9e02ea75ee0db&clan_id=%s" % (id)
+        response = requests.get(api_url)
+        json = response.json()['data'][str(id)]
+        self.skirmish['fb_6'] = json['fb_elo_rating_6']
+        self.skirmish['fb_8'] = json['fb_elo_rating_8']
+        self.skirmish['fb_10'] = json['fb_elo_rating_10']
+        self.global_map['gm_6'] = json['gm_elo_rating_6']
+        self.global_map['gm_8'] = json['gm_elo_rating_8']
+        self.global_map['gm_10'] = json['gm_elo_rating_10']
+
--- main.py
+++ main.py
@@ -1,17 +1,53 @@
 import requests
 import json
 from clan import Clan
+import discord
+from discord.ext import commands
 
 clan = Clan()
+#client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
-def retrieveClan(tag):
+def getClan(tag):
     api_url = "https://api.worldoftanks.eu/wot/clans/list/?application_id=0ecfda2435a084d16fa9e02ea75ee0db&search=%s" % (tag)
     response = requests.get(api_url)
-    json = response.json()['data'][0]
-    clan.id = json['clan_id']
-    clan.name = json['name']
-    clan.tag = json['tag']
-    clan.emblem_url = json['emblems']['x256']['wowp']
-    print(clan.tag)
+    if(response.json()['status'] == "ok" and
+       response.json()['meta']['total'] == 1):
+        json = response.json()['data'][0]
+        clan.id = json['clan_id']
+        clan.name = json['name']
+        clan.tag = json['tag']
+        clan.emblem_url = json['emblems']['x256']['wowp']
+        clan.getDetails(clan.id)
+        clan.getRating(clan.id)
+    else:
+        print("Invalid clan tag")
 
-retrieveClan('TNKCS')
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+
+#     if message.content.startswith('$hello'):
+#         await message.channel.send('Hello!')
+
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
+
+@bot.command()
+async def FindClan(ctx, arg):
+    getClan(arg)
+    await ctx.send(clan.name)
+
+@FindClan.error
+async def info_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('No clan tag passed.')
+
+@bot.command()
+async def PrintRating(ctx, arg):
+    getClan(arg)
+    await ctx.send(clan.skirmish['fb_6'])
+
+bot.run('NjU0ODA1Nzk2NDk1OTQ5ODM3.XfK5jQ.Fh1hp5E9yZremGH9oGdf6YuPVBI')
