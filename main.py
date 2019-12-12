893cdd19c6ee0e459b7825e025183cef37f850cb
Simon <simon.brol@outlook>
2019-12-12 23:41:07 +0100

    Removed unnecessary code

--- main.py
+++ main.py
@@ -23,14 +23,6 @@ def getClan(tag):
     else:
         print("Invalid clan tag")
 
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
-
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
