aa70ad0b8c1c95f68a09a4e47a04a44382aac53a
Simon <simon.brol@outlook>
2019-12-15 12:38:12 +0100

    Daily update

--- clan.py
+++ clan.py
@@ -10,6 +10,9 @@ class Clan:
     emblem_url = ""
     color = ""
     created_at = ""
+    created_by = ""
+    activity = ""
+    commander = ""
     description = ""
     members_count = 0
     motto = ""
@@ -20,11 +23,17 @@ class Clan:
         'fb_8': {'value': '', 'rank': '', 'rank_delta': ''},
         'fb_10': {'value': '', 'rank': '', 'rank_delta': ''}
     }
+    sm_6_str = ""
+    sm_8_str = ""
+    sm_10_str = ""
     global_map = {
         'gm_6': {'value': '', 'rank': '', 'rank_delta': ''},
         'gm_8': {'value': '', 'rank': '', 'rank_delta': ''},
         'gm_10': {'value': '', 'rank': '', 'rank_delta': ''}
     }
+    gm_6_str = ""
+    gm_8_str = ""
+    gm_10_str = ""
 
     #functions
     def getDetails(self, id):
@@ -37,6 +46,8 @@ class Clan:
         self.members_count = json['members_count']
         self.motto = json['motto']
         self.updated_at = json['updated_at']
+        self.created_by = json['creator_name']
+        self.commander = json['leader_name']
 
     def getRating(self, id):
         api_url = "https://api.worldoftanks.eu/wot/clanratings/clans/?application_id=0ecfda2435a084d16fa9e02ea75ee0db&clan_id=%s" % (id)
@@ -48,4 +59,11 @@ class Clan:
         self.global_map['gm_6'] = json['gm_elo_rating_6']
         self.global_map['gm_8'] = json['gm_elo_rating_8']
         self.global_map['gm_10'] = json['gm_elo_rating_10']
+
+        self.sm_6_str = "Rating: **" + str(self.skirmish['fb_6']['value']) + "**\nRank: **" + str(self.skirmish['fb_6']['rank']) + "**\nRank Change: **" + str(self.skirmish['fb_6']['rank_delta']) + "**"
+        self.sm_8_str = "Rating: **" + str(self.skirmish['fb_8']['value']) + "**\nRank: **" + str(self.skirmish['fb_8']['rank']) + "**\nRank Change: **" + str(self.skirmish['fb_8']['rank_delta']) + "**"
+        self.sm_10_str = "Rating: **" + str(self.skirmish['fb_10']['value']) + "**\nRank: **" + str(self.skirmish['fb_10']['rank']) + "**\nRank Change: **" + str(self.skirmish['fb_10']['rank_delta']) + "**"
+        self.gm_6_str = "Rating: **" + str(self.global_map['gm_6']['value']) + "**\nRank: **" + str(self.global_map['gm_6']['rank']) + "**\nRank Change: **" + str(self.global_map['gm_6']['rank_delta']) + "**"
+        self.gm_8_str = "Rating: **" + str(self.global_map['gm_8']['value']) + "**\nRank: **" + str(self.global_map['gm_8']['rank']) + "**\nRank Change: **" + str(self.global_map['gm_8']['rank_delta']) + "**"
+        self.gm_10_str = "Rating: **" + str(self.global_map['gm_10']['value']) + "**\nRank: **" + str(self.global_map['gm_10']['rank']) + "**\nRank Change: **" + str(self.global_map['gm_10']['rank_delta']) + "**"
 
--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import requests
 import json
 from clan import Clan
 import discord
-from discord.ext import commands
+from discord.ext import tasks, commands
 from datetime import datetime
+import asyncio
 
 clan = Clan()
-#client = discord.Client()
+send_time='06:00'
 bot = commands.Bot(command_prefix='!')
 
 def getClan(tag):
     api_url = "https://api.worldoftanks.eu/wot/clans/list/?application_id=0ecfda2435a084d16fa9e02ea75ee0db&search=%s" % (tag)
     response = requests.get(api_url)
     if(response.json()['status'] == "ok" and
-       response.json()['meta']['total'] == 1):
+       response.json()['meta']['count'] >= 1):
         json = response.json()['data'][0]
         clan.id = json['clan_id']
         clan.name = json['name']
@@ -22,47 +23,94 @@ def getClan(tag):
         clan.getDetails(clan.id)
         clan.getRating(clan.id)
         print(clan.id)
+        return True
     else:
-        print("Invalid clan tag")
+        return False
 
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
-@bot.command()
+@tasks.loop(seconds=50)
+async def sendRatingDaily(id: int, tag: str):
+    channel = bot.get_channel(id)
+    now = datetime.strftime(datetime.now(), '%H:%M')
+    if(now == send_time):
+        if(getClan(tag)):
+            color_str = "0x" + clan.color[1:]
+            embed = discord.Embed(title=clan.name, description=clan.motto, color=int(color_str, 16))
+            embed.set_thumbnail(url=clan.emblem_url)
+            embed.add_field(name="\u200b", value="Skirmish Rating:", inline=False)
+            embed.add_field(name="T6", value=clan.sm_6_str, inline=True)
+            embed.add_field(name="T8", value=clan.sm_8_str, inline=True)
+            embed.add_field(name="T10", value=clan.sm_10_str, inline=True)
+            embed.add_field(name="\u200b", value="Global Map Rating:", inline=False)
+            embed.add_field(name="T6", value=clan.gm_6_str, inline=True)
+            embed.add_field(name="T8", value=clan.gm_8_str, inline=True)
+            embed.add_field(name="T10", value=clan.gm_10_str, inline=True)
+            footer_text = "Updated Statistics at: " + str(datetime.fromtimestamp(clan.updated_at))
+            embed.set_footer(text=footer_text)
+            await channel.send(embed=embed)
+        else:
+            await channel.send("Invalid clan tag")
+
+@bot.command(name="find", help="Finds a clan.", description="Finds a clan based on the tag and shows various data of the clan. \n Example: '!find TNKCS'")
 async def find(ctx, arg):
-    getClan(arg)
-    await ctx.send(clan.id)
-    await ctx.send(clan.name)
+    if(getClan(arg)):
+        color_str = "0x" + clan.color[1:]
+        embed = discord.Embed(title=clan.name, description=clan.motto, color=int(color_str, 16))
+        embed.set_author(name=arg)
+        embed.set_thumbnail(url=clan.emblem_url)
+        embed.add_field(name="\u200b", value=clan.description, inline=False)
+        embed.add_field(name="Commander", value=clan.commander, inline=True)
+        embed.add_field(name="Member Count", value=clan.members_count, inline=True)
+        embed.add_field(name="Created at", value=str(datetime.fromtimestamp(clan.created_at)), inline=False)
+        embed.add_field(name="Creator", value=clan.created_by, inline=True)
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send("Invalid clan tag")
 
-@find.error
-async def info_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('No clan tag passed.')
-
-@bot.command()
+@bot.command(name="rating", help="Shows skirmish/global map rating.", description="Retrieves global map and skirmish data of a clan by tag. \n Example: '!rating TNKCS'")
 async def rating(ctx, arg):
-    getClan(arg)
-    color_str = "0x" + clan.color[1:]
-    skirmish_t6 = "Rating: **" + str(clan.skirmish['fb_6']['value']) + "**\nRank: **" + str(clan.skirmish['fb_6']['rank']) + "**\nRank Change: **" + str(clan.skirmish['fb_6']['rank_delta']) + "**"
-    skirmish_t8 = "Rating: **" + str(clan.skirmish['fb_8']['value']) + "**\nRank: **" + str(clan.skirmish['fb_8']['rank']) + "**\nRank Change: **" + str(clan.skirmish['fb_8']['rank_delta']) + "**"
-    skirmish_t10 = "Rating: **" + str(clan.skirmish['fb_10']['value']) + "**\nRank: **" + str(clan.skirmish['fb_10']['rank']) + "**\nRank Change: **" + str(clan.skirmish['fb_10']['rank_delta']) + "**"
-    global_map_t6 = "Rating: **" + str(clan.global_map['gm_6']['value']) + "**\nRank: **" + str(clan.global_map['gm_6']['rank']) + "**\nRank Change: **" + str(clan.global_map['gm_6']['rank_delta']) + "**"
-    global_map_t8 = "Rating: **" + str(clan.global_map['gm_8']['value']) + "**\nRank: **" + str(clan.global_map['gm_8']['rank']) + "**\nRank Change: **" + str(clan.global_map['gm_8']['rank_delta']) + "**"
-    global_map_t10 = "Rating: **" + str(clan.global_map['gm_10']['value']) + "**\nRank: **" + str(clan.global_map['gm_10']['rank']) + "**\nRank Change: **" + str(clan.global_map['gm_10']['rank_delta']) + "**"
-    embed = discord.Embed(title=clan.name, description=clan.motto, color=int(color_str, 16))
-    embed.set_thumbnail(url=clan.emblem_url)
-    embed.add_field(name="Skirmish Ratings:", value="---", inline=False)
-    embed.add_field(name="T6", value=skirmish_t6, inline=True)
-    embed.add_field(name="T8", value=skirmish_t8, inline=True)
-    embed.add_field(name="T10", value=skirmish_t10, inline=True)
-    embed.add_field(name="Global Map Ratings:", value="---", inline=False)
-    embed.add_field(name="T6", value=global_map_t6, inline=True)
-    embed.add_field(name="T8", value=global_map_t8, inline=True)
-    embed.add_field(name="T10", value=global_map_t10, inline=True)
-    footer_text = "Updated Statistics at: " + str(datetime.fromtimestamp(clan.updated_at))
-    embed.set_footer(text=footer_text)
-    await ctx.send(embed=embed)
-    # await ctx.send(clan.skirmish['fb_6'])
+    if(getClan(arg)):
+        color_str = "0x" + clan.color[1:]
+        embed = discord.Embed(title=clan.name, description=clan.motto, color=int(color_str, 16))
+        embed.set_thumbnail(url=clan.emblem_url)
+        embed.add_field(name="\u200b", value="Skirmish Rating:", inline=False)
+        embed.add_field(name="T6", value=clan.sm_6_str, inline=True)
+        embed.add_field(name="T8", value=clan.sm_8_str, inline=True)
+        embed.add_field(name="T10", value=clan.sm_10_str, inline=True)
+        embed.add_field(name="\u200b", value="Global Map Rating:", inline=False)
+        embed.add_field(name="T6", value=clan.gm_6_str, inline=True)
+        embed.add_field(name="T8", value=clan.gm_8_str, inline=True)
+        embed.add_field(name="T10", value=clan.gm_10_str, inline=True)
+        footer_text = "Updated Statistics at: " + str(datetime.fromtimestamp(clan.updated_at))
+        embed.set_footer(text=footer_text)
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send("Invalid clan tag")
+        
+@bot.command(name="skirmish", help="Shows skirmish data by tier.", description="Retrieves skirmish data of a specific tier from a clan by tag. \n Example: '!skirmish TNKCS t6'")
+async def skirmish(ctx, tag: str, tier: str):
+    if(getClan(tag)):
+        color_str = "0x" + clan.color[1:]
+        embed = discord.Embed(title=clan.name, description=clan.motto, color=int(color_str, 16))
+        embed.add_field(name="\u200b", value="Skirmish Rating:", inline=False)
+        if(tier == '6' or tier == 't6'):
+            embed.add_field(name="T6", value=clan.sm_6_str, inline=True)
+        elif(tier == '8' or tier == 't8'):
+            embed.add_field(name="T8", value=clan.sm_8_str, inline=True)
+        elif(tier == '10' or tier == 't10'):
+            embed.add_field(name="T6", value=clan.sm_10_str, inline=True)
+        embed.set_thumbnail(url=clan.emblem_url)
+        footer_text = "Updated Statistics at: " + str(datetime.fromtimestamp(clan.updated_at))
+        embed.set_footer(text=footer_text)
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send("Invalid clan tag")
 
-bot.run('NjU0ODA1Nzk2NDk1OTQ5ODM3.XfUBnQ.oyhe7y3oKeB3SOGqsCawedlts8o')
+@bot.command(name="setDaily", help="Set information for daily updates.", description="Sets the channel the bot is allowed to speak in by itself, it also sets the clan tag you want your daily ratings from.\n Example: '!setDaily TNKCS'")
+async def setDaily(ctx, arg):
+    sendRatingDaily.start(ctx.channel.id, arg)
+
+bot.run('NjU0ODA1Nzk2NDk1OTQ5ODM3.XfU9vw.DdjahgKeXHLOD_oJU3WyyyWolNM')
